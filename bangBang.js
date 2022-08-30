68ca14640a2f968a411a002b97fad0ded9cb38f0
Victoria Beasley <iesnoth91@gmail.com>
2022-08-30 18:50:57 -0500

    made a passengers class

--- bangBang.js
+++ bangBang.js
@@ -1,4 +1,5 @@
 const VehicleModule = require('./vehicle').Vehicle
+const PassPeople = require('./passengerClass').Passenger
 
 class ChittyChitty extends VehicleModule {
     constructor(make, model, year, color, mileage){
@@ -6,7 +7,11 @@ class ChittyChitty extends VehicleModule {
         this.canFly = false;
         this.maxPassengers = 4;
         this.canFloat = false;
-        
+
     }
 
 }
+
+//Passengers of Chitty Chitty Bang Bang
+let mike = new PassPeople('Mike',32, 'male')
+console.log(mike)
