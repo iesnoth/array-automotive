805dabc9ae50992f676df1d30d36794f0ade0f94
Victoria Beasley <iesnoth91@gmail.com>
2022-08-30 19:31:45 -0500

    more people stuff

--- passengerClass.js
+++ passengerClass.js
@@ -8,7 +8,35 @@ class Passenger {
         cast.push(this)
     }
 
+    driveWell(){
+        if(this.gender === 'male' && this.age >= 18){
+            console.log(`${this.name} can drive well, but is it worth it?`)
+        }
+        else if (this.gender === 'female' && this.age >= 18){
+            console.log(`${this.name} can't drive well for the sake of comedy.`)
+        }
+        else{
+            console.log(`${this.name} is a child, thus cannot drive.`)
+        }}
 }
+
+
+
+// class Driver extends Passenger{
+//     constructor(name, age, gender){
+//         super(name, age, gender)
+//         canDrive = true
+//         cast.push(this)
+//     }
+
+//     driveWell(){
+//         if(this.gender === 'male'){
+//             console.log(`${this.name} can drive well, but is it worth it?`)
+//         }
+//         else{
+//             console.log(`${this.name} can't drive well for the sake of comedy.`)
+//         }}
+// }
 
 let mrPotts = new Passenger('Caractacus Potts',30, 'male')
 let truly = new Passenger('Truly Scrumptious',26,'female')
@@ -16,6 +44,11 @@ let boyPotts = new Passenger('Jeremy Potts',10, 'male')
 let girlPotts = new Passenger('Jemima Potts',8,'female')
 
 console.log(cast)
+truly.driveWell()
+boyPotts.driveWell()
+
+
+
 module.exports = {
     Passenger
 }
